dc8a5004107708300203557a528658f63519c436
catalina <cata.acevedo01@gmail.com>
2020-07-22 12:40:59 -0500

    ultimos cambios

--- ejemplo.js
+++ ejemplo.js
@@ -2,9 +2,10 @@
 let p,a
 function calculo(p, a) //definicion de variables
 {
-    return p/(a);
+    return p/(a^2);
 }
 
 let peso =prompt('Ingresa tu peso en kilogramos'); //asignar valor a peso
 let altura =prompt('Ingresa tu altura en metros'); //asignar valor a altura
-
+let resultado =calculo(peso, altura)
+alert (resultado)
