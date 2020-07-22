488f5f273a48226960321c9205346b79753b25af
catalina <cata.acevedo01@gmail.com>
2020-07-22 12:09:31 -0500

    cambios

--- ejemplo.js
+++ ejemplo.js
@@ -1,6 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        
-    </head>
-</html>
+// calcular el IMC
+let p,a
+function calculo(p, a) //definicion de variables
+{
+    return p/(a);
+}
+
+let peso =prompt('Ingresa tu peso en kilogramos'); //asignar valor a peso
+let altura =prompt('Ingresa tu altura en metros'); //asignar valor a altura
+
